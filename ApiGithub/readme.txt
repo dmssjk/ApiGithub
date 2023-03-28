1f5f0c3a3b7db5fcf4fd9cfe114ef2af41c60f13
Daniel <dmssjk@gmail.com>
2023-03-28 16:14:36 -0300

    big fix nome de usuario

---
+++ ApiGithub/readme.txt
@@ -0,0 +1,8 @@
+## instalando as dependencias 
+
+npm create vite@latest
+react / ts
+
+npm install 
+
+npm run dev 
--- ApiGithub/src/routes/home.tsx
+++ ApiGithub/src/routes/home.tsx
@@ -12,12 +12,22 @@ const home = () => {
 
     const data = await res.json();
 
-    const {avatar_url, login, location, followers, following} = data
+    const { avatar_url, login, location, followers, following } = data;
+    const userData: UserProps = {
+      avatar_url,
+      login,
+      location,
+      followers,
+      following,
+    };
+    setUser(userData);
+
   };
 
   return (
     <div>
       <Search loadUser={loadUser} />
+      {user && <p>{}user.login}</p>}
     </div>
   );
 };
